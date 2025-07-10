3b5894539d929ce19b735e4817db993ea480c231
EPI-JHS <j.humphreys@epiconcept.fr>
2025-07-10 10:15:40 +0200

    Adding tests

--- R/Imports.R
+++ R/Imports.R
@@ -1,4 +1,4 @@
-#' @importFrom data.table data.table copy setDT fread setorder
+#' @importFrom data.table data.table copy setDT fread setorder fifelse %chin%
 #' @import dplyr
 #' @import openxlsx
 #' @importFrom tidyr pivot_wider
